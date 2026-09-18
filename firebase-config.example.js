a2a8631eaf4c194d10179cc0a53500274085db6d
shadow-strikers1 <hssjjsjshzzhhshsz@gmail.com>
2026-09-18 13:57:14 -0300

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBptcBXdYD8kfZtZIFoWmv0outPiuuKbqQ",
-  authDomain: "chat-livre-ai-v2.firebaseapp.com",
-  projectId: "chat-livre-ai-v2",
-  storageBucket: "chat-livre-ai-v2.firebasestorage.app",
-  messagingSenderId: "367169410928",
-  appId: "1:367169410928:web:48d0b5fb5221443ebd61b5"
-};
